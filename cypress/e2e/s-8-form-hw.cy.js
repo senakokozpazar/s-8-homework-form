a4f128bcfa72804b89798ebe686d49010eab376d
Sena Kokoz Pazar <senakokoz@gmail.com>
2024-07-21 23:30:04 +0300

    last cypress test

--- cypress/e2e/s-8-form-hw.cy.js
+++ cypress/e2e/s-8-form-hw.cy.js
@@ -17,6 +17,21 @@ describe('Register Page', () => {
       cy.get('[data-cy="password-input"]').type("1234");
       cy.contains(errorMessages.password)
     });
+    it('button is disabled for unvalidated inputs ', () => {
+      cy.visit('http://localhost:5173/');
+      cy.get('[data-cy="password-input"]').type("1234");
+      cy.get('[data-cy="button"]').should("be.disabled")
+    });
+  });
+  describe('Form inputs validated', () =>{
+    it('button enabled for validated inputs ', () => {
+      cy.visit('http://localhost:5173/');
+      cy.get('[data-cy="name-input"]').type("sena");
+      cy.get('[data-cy="email-input"]').type("sena@wit.com.tr");
+      cy.get('[data-cy="password-input"]').type("1234Aa*");
+      cy.get('[data-cy="button"]').should("be.enabled")
+    });
+    
   })
 
 })
